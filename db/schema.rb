16fb056ea2eee8675b9dbaf05f7ed5f2cee2b437
Carson Cole <carson.cole@gmail.com>
2020-10-21 14:55:58 -0700

    minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_204925) do
+ActiveRecord::Schema.define(version: 2020_10_21_213001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2020_10_21_204925) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "cookie_id"
     t.integer "votes_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
