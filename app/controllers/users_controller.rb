c6072cc1891d301ff998d86249c862a1e61a56a9
Carson Cole <carson.cole@gmail.com>
2020-10-23 10:41:43 -0700

    fixed flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < Clearance::UsersController
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    new_params = user_params
+    new_params = user_params.reject! { |k,v| v.blank? }
+    if current_user.update(new_params)
+      redirect_to edit_user_path(current_user), notice: 'Your account has been updated'
+    else
+      render :edit
+    end
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :troop_id)
   end
