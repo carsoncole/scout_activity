b91fd5d3633670bf7d0b8b2c13ff6cced22c5b51
Carson Cole <carson.cole@gmail.com>
2020-10-27 19:41:00 -0700

    renamed info -> about

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     resources :votes, only: [:index, :create, :destroy]
   end
 
-  get 'info' => 'home#info', as: 'info'
+  get 'info' => 'home#about', as: 'about'
 end
