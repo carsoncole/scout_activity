9df0597ae36ac7c1e619a96a188fdee7e4914e5a
Carson Cole <carson.cole@gmail.com>
2020-11-10 07:12:13 -0800

    more tests

--- test/system/activities_test.rb
+++ test/system/activities_test.rb
@@ -1,10 +1,86 @@
 require "application_system_test_case"
 
 class ActivitiesTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    @activity = create(:activity)
-    visit unit_activities_url(@activity.unit)
-    assert_selector "h1", text: @activity.unit.name + " Activity Vote"
+  test "activities index (not logged in)" do
+    activity = create(:activity)
+    visit unit_activities_url(activity.unit)
+    assert_selector "h1", text: activity.unit.name + " Activity Vote"
+    has_button? "Sign in or sign up to vote"
+    assert_selector "ul.activities li", count: 1
+    has_link? activity.name
+    click_link activity.name
+  end
+
+  test "showing an activity (not logged in)" do
+    activity = create(:activity)
+    visit unit_activity_url(activity.unit, activity)
+    assert_selector "h1", text: activity.name
+    assert_no_selector "#activity-edit-link"
+    assert_no_selector "#copy-activity-link"
+    assert_no_selector "#archive-button"
+    assert_no_selector "#destroy-button"
+  end
+
+  test "editing an activity (as author)" do
+    sign_in
+    activity = create(:activity, author: @user)
+    visit unit_activity_path(activity.unit, activity)
+
+    assert_selector "#activity-edit-link"
+    assert_no_selector "#copy-activity-link"
+    assert_selector "#archive-button"
+    assert_selector "#destroy-button"
+
+    assert_text "Activity length: " + activity.duration_days
+    click_on "activity-edit-link"
+    fill_in "Number of days", with: '5'
+    fill_in "What's a good title for your activity idea?", with: activity.name + 'New'
+    click_on "Update Activity"
+    assert_text "Activity was successfully updated."
+    assert_selector "h1", text: activity.name
+    assert_text "Activity length: 5 days"
+  end
+
+  test "editing an activity (as admin)" do
+    activity = create(:activity)
+    admin = create(:admin_user, unit: activity.unit)
+    sign_in(admin)
+    visit unit_activity_url(activity.unit, activity)
+
+    assert_selector "#activity-edit-link"
+    assert_selector "#copy-activity-link"
+    assert_selector "#archive-button"
+    assert_selector "#destroy-button"
+
+    assert_text "Activity length: " + activity.duration_days
+    click_on "activity-edit-link"
+    fill_in "Number of days", with: '5'
+    fill_in "What's a good title for your activity idea?", with: activity.name + 'New'
+    click_on "Update Activity"
+    assert_text "Activity was successfully updated."
+    assert_selector "h1", text: activity.reload.name
+    assert_text "Activity length: 5 days"
+  end
+
+  test "editing an activity (as owner)" do
+    activity = create(:activity)
+    admin = create(:owner_user, unit: activity.unit)
+    sign_in(admin)
+    visit unit_activity_url(activity.unit, activity)
+
+    assert_selector "#activity-edit-link"
+    assert_selector "#copy-activity-link"
+    assert_selector "#archive-button"
+    assert_selector "#destroy-button"
+
+    assert_text "Activity length: " + activity.duration_days
+    click_on "activity-edit-link"
+    fill_in "Number of days", with: '10'
+    fill_in "What's a good title for your activity idea?", with: activity.name + 'NewAgain'
+    click_on "Update Activity"
+    assert_text "Activity was successfully updated."
+    assert_selector "h1", text: activity.reload.name
+    assert_text "Activity length: 10 days"
   end
 
   test "creating an activity" do
@@ -21,56 +97,87 @@ class ActivitiesTest < ApplicationSystemTestCase
     fill_in "Number of days", with: "3"
     check "Swimming"
     check "Biking"
+    check "Hiking"
+    check "Flying"
     click_on "Create Activity"
     assert_text "Activity was successfully created."
   end
 
-  test "viewing an activity" do
-    @activity = create(:activity)
+  test "creating an activity with all icons" do
+    sign_in
+    visit new_unit_activity_path(@user.unit)
+    fill_in "What's a good title for your activity idea?", with: Faker::Lorem.sentence(word_count: 5)
+    fill_in "A brief 2-3 lines summarizing your idea", with: Faker::Lorem.sentence(word_count: 3) 
 
-    visit unit_activity_path(@activity.unit, @activity)
-    assert_selector "h1", text: @activity.name
-    assert_no_selector "#activity-edit-link"
-    assert_no_selector "#copy-activity-link"
-    assert_no_selector "#archive-button"
-    assert_no_selector "#destroy-button"
+    check "Hiking"
+    check "Camping"
+    check "Swimming"
+    check "Flying"
+    check "Community service"
+    check "Biking"
+    check "Cooking"
+    check "Merit badge"
+    check "Fundraising"
+    check "International Scouting"
+    check "Virtual"
+    check "Game"
+    check "High Adventure"
+    click_on "Create Activity"
+    assert_text "Activity was successfully created."
+
+    assert_selector "#hiking-icon"
+    assert_selector "#camping-icon"
+    assert_selector "#swimming-icon"
+    assert_selector "#biking-icon"
+    assert_selector "#flying-icon"
+    assert_selector "#cooking-icon"
+    assert_selector "#merit-badge-icon"
+    assert_selector "#fundraising-icon"
+    assert_selector "#game-icon"
+    assert_selector "#virtual-icon"
+    assert_selector "#community-service-icon"
+    assert_selector "#international-icon"
+    assert_selector "#high-adventure-icon"
   end
 
-  test "updating an activity" do
+  test "archiving/unarchiving an activity" do
     sign_in
     activity = create(:activity, author: @user)
-    visit unit_activity_path(activity.unit, activity)
-    assert_text "Activity length: " + activity.duration_days
-    click_on "activity-edit-link"
-    fill_in "Number of days", with: '5'
-    fill_in "What's a good title for your activity idea?", with: activity.name + 'New'
-    click_on "Update Activity"
-    assert_text "Activity was successfully updated."
-    assert_selector "h1", text: activity.name
-    assert_text "Activity length: 5 days"
 
-    assert_selector "#activity-edit-link"
-    assert_no_selector "#copy-activity-link"
-    assert_selector "#archive-button"
-    assert_selector "#destroy-button"
-  end
+    user = create(:user, unit: @user.unit)
+    create_list(:vote, 6, user: user, activity: activity)
 
-  test "archiving an activity" do
-    sign_in
-    activity = create(:activity, author: @user)
-    create_list(:vote, 5, activity: activity, user: @user)
-    click_on "Vote"
-    assert_link activity.name
-    # assert_selector "table .activities tbody tr", count: 1
+    assert_equal 1, activity.unit.activities.votable.count
+    assert_equal 0, activity.unit.activities.archived.count
+
     visit unit_activity_path(activity.unit, activity)
+    assert_selector "#activity-vote-count", text: '6'
+
+    assert_equal 1, activity.unit.activities.votable.count
+    assert_equal 0, activity.unit.activities.archived.count
+    assert_equal 6, activity.votes.count
+
     click_button 'Archive'
     assert_selector "#flash", text: 'Activity is archived, and no longer in the activity voting list.'
+
+    assert_selector "#unarchive-button"
+    assert_no_selector "#archive-button"
+
     assert_selector "#activity-vote-count", text: '0'
-    click_on "Vote"
-    assert_no_link activity.name
-    visit unit_activity_path(activity.unit, activity)
-    click_button 'Unarchive'
-    assert_selector "#flash", text: 'Activity is unarchived and showing in the activity voting list.'
+
+    click_on 'Vote'
+    assert_selector "h3", text: "Archived"
+
+    visit unit_activity_path(@user.unit, activity)
+    click_button "Unarchive"
+
+    assert_equal 1, activity.reload.unit.activities.votable.count
+    assert_equal 0, activity.unit.activities.archived.count
+    assert_equal 0, activity.votes.count
+  end
+  
+  test "voting on an activity" do
+    # votes are handled in votes_test.rb
   end
 
   test "destroying a activity" do
