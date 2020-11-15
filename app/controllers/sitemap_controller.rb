662702ee3178ff8a3cb505b25209b155fad736f6
Carson Cole <carson.cole@gmail.com>
2020-11-15 10:23:03 -0800

    fixed date

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -7,6 +7,6 @@ class SitemapController < ApplicationController
     @last_root_page_update = time if time > @last_root_page_update
     time = Unit.order(updated_at: :desc).first&.updated_at&.strftime("%Y-%m-%d")
     @last_root_page_update = time if time && time > @last_root_page_update
-    @last_troop_ideas_update = Unit.example.first.activities.troop.order(updated_at: :asc).first.updated_at
+    @last_troop_ideas_update = Unit.example.first.activities.troop.order(updated_at: :desc).first.updated_at
   end
 end
